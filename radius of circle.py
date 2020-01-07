47cd395b2a6acb0b37fc60685f29678545940b95
unknown <student@CSELAB1-14.vdp.ac.in>
2020-01-07 15:07:21 +0530

    input-print name and age

---
+++ radius of circle.py
@@ -0,0 +1,2 @@
+radius = input('Enter radius ')
+print(float(radius)**2*3.14)
---
+++ reversae string.py
@@ -0,0 +1,4 @@
+first_name = input('Enter your first name ')[::-1]
+last_name = input('Enter your last name ')[::-1]
+
+print(first_name,last_name)
